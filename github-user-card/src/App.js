a552e262fc287600d86c01d723c1753564f14381
Landon Turner <lturner@LTurner-MBP.lan1>
2020-03-10 16:35:09 -0400

    rendering my data to the page

--- github-user-card/src/App.js
+++ github-user-card/src/App.js
@@ -5,17 +5,37 @@ import './App.css';
 
 class App extends React.Component {
   state = {
-    data: []
-  }
+    landoData: []
+  };
+
 
   componentDidMount() {
     axios.get('https://api.github.com/users/landoDev')
-    .then(response => console.log(response))
+    .then(response =>{
+      //response.data
+      this.setState({
+        landoData: response.data
+      });
+      console.log('data', this.state.landoData)
+    } );
+    
   }
   render(){
     return (
       <div className="App">
         <header><h1>Lando's Github Squad</h1></header>
+        <div>
+          <h2>{this.state.landoData.name}</h2>
+          <img src={this.state.landoData.avatar_url}></img>
+          <p>{this.state.landoData.location}</p>
+          <p>{this.state.landoData.bio}</p>
+          <p>Username: {this.state.landoData.login}</p>
+          <label>URL: 
+            <a href={this.state.landoData.html_url}>{this.state.landoData.html_url}</a>
+          </label>
+
+
+        </div>
         
       </div>
     );
