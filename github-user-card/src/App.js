a16a9dd8e012253b180260d26856276d9367738e
Landon Turner <lturner@LTurner-MBP.lan1>
2020-03-10 17:24:35 -0400

    styling done mvp achieved

--- github-user-card/src/App.js
+++ github-user-card/src/App.js
@@ -1,8 +1,65 @@
 import React from 'react';
 import axios from 'axios';
-import logo from './logo.svg';
+import styled from 'styled-components'
 import './App.css';
 
+const AppDiv= styled.div`
+header{
+}
+.user-card{
+  background-color: #64696C;
+  border: 5px outset #556567;
+  border-radius: 15px;
+  padding: 2% 0;
+  width: 30%;
+  margin: 2% 0;
+  h2{
+    font-size: 2rem;
+  }
+  img{
+    border-radius: 50%;
+    border: 5px outset #394A59;
+  }
+  a{
+    text-decoration: none;
+    color: #BC1E22;
+    text-shadow: 2px 0 2px #394A59;
+  }
+}
+.user-card.title-card{
+  margin:0 auto;
+}
+.followers{
+  display: flex;
+  justify-content: space-evenly;
+  flex-flow: wrap;
+}
+
+`;
+// const UserCard = styled.div`
+//   background-color: #64696C;
+//   border: 5px outset #556567;
+//   width: 50%;
+//   .title-card{
+//     margin: 0 auto;
+//   }
+//   h2{
+//     font-size: 2rem;
+//   }
+//   a{
+//     text-decoration: none;
+//     color: #BC1E22;
+//     text-shadow: 2px 0 2px #394A59;
+//   }
+// `;
+
+const FollowersDiv = styled.div`
+  .followers{
+    display: flex;
+    justify-content: space-evenly;
+    flex-flow: wrap;
+  }
+`;
 class App extends React.Component {
   state = {
     landoData: [],
@@ -20,7 +77,7 @@ class App extends React.Component {
     })
     .then(axios.get('https://api.github.com/users/landoDev/followers')
       .then(response=> {
-        // console.log(response)
+        // console.log(response.data)
         this.setState({
           followerData: response.data
         })
@@ -29,9 +86,9 @@ class App extends React.Component {
   }
   render(){
     return (
-      <div className="App">
-        <header><h1>Lando's Github Squad</h1></header>
-        <div>
+      <AppDiv className="App">
+        <header><h1>Lando's Github Legion</h1></header>
+        <div className='user-card title-card'>
           <h2>{this.state.landoData.name}</h2>
           <img src={this.state.landoData.avatar_url}></img>
           <p>{this.state.landoData.location}</p>
@@ -41,24 +98,22 @@ class App extends React.Component {
             <a href={this.state.landoData.html_url}>{this.state.landoData.html_url}</a>
           </label>
         </div>
-        <div>
+        <div className='followers'>
           {this.state.followerData.map(follower => {
             return(
-              <div key={follower.id}>
+              <div key={follower.id} className='user-card'>
               <h2>{follower.name}</h2>
               <img src={follower.avatar_url}></img>
               <p>{follower.location}</p>
               <p>{follower.bio}</p>
               <p>Username: {follower.login}</p>
-              <label>URL: 
-                <a href={follower.html_url}>{follower.html_url}</a>
-              </label>
+              <a href={follower.html_url}>{follower.html_url}</a>
             </div>
             );
           })}
         </div>
         
-      </div>
+      </AppDiv>
     );
   }
 
